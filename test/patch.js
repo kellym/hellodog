bcbd4057dfd31e19a398258c9675416720d6921d
Kelly Martin <kellymartinv@gmail.com>
2017-10-23 15:29:22 -0500

    rename socketLogger to socketRecorder

--- test/patch.js
+++ test/patch.js
@@ -1,19 +1,19 @@
 const tap = require('tap');
 const net = require('net');
 const originalSocket = net.Socket;
-const socketLogger = require('../index');
+const socketRecorder = require('../index');
 
 tap.test('should not patch until patch() is called', (t) => {
   t.equal(net.Socket, originalSocket);
-  socketLogger.patch();
+  socketRecorder.patch();
   t.notEqual(net.Socket, originalSocket);
   t.end();
 });
 
 tap.test('should unpatch when unpatch() is called', (t) => {
-  socketLogger.patch();
+  socketRecorder.patch();
   t.notEqual(net.Socket, originalSocket);
-  socketLogger.unpatch();
+  socketRecorder.unpatch();
   t.equal(net.Socket, originalSocket);
   t.end();
 });
