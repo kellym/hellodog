01e57c55eaf4f83434909a79b9e3e703372c25ae
Kelly Martin <kellymartinv@gmail.com>
2022-04-29 13:16:11 -0500

    Fix test and polyfill

--- test/ftp.spec.js
+++ test/ftp.spec.js
@@ -5,31 +5,31 @@ const FtpServer = require('./support/ftp');
 const FtpClient = require('ftp');
 const getPort = require('./support/get-port');
 
-test('tracks ftp server', done => {
-  getPort().then(async port => {
-    const options = {
-      port,
-      host: '127.0.0.1',
-      user: 'andrew',
-      password: 'bird'
-    };
-    const server = FtpServer(options);
+test('tracks ftp server', async done => {
+  const port = await getPort();
+  const options = {
+    port,
+    host: '127.0.0.1',
+    user: 'andrew',
+    password: 'bird'
+  };
+  const server = FtpServer(options);
 
-    const log = await track(callback => {
-      const client = new FtpClient();
-      client.on('ready', () => {
-        client.put('MyInformation', 'data.txt', (err) => {
-          client.end();
-          server.close();
-          callback(err);
-        });
+  const log = await track(callback => {
+    const client = new FtpClient();
+    client.on('ready', () => {
+      client.put('MyInformation', 'data.txt', (err) => {
+        client.end();
+        server.close();
+        callback(err);
       });
-      client.on('error', (err) => {
-        done(err);
-      });
-      client.connect(options);
     });
-    assert.equal(log[0].events.filter(e => e.request && e.request.indexOf(options.user) !== -1).length, 1);
-    assert.equal(log[0].events.filter(e => e.request && e.request.indexOf(options.password) !== -1).length, 1);
+    client.on('error', (err) => {
+      done(err);
+    });
+    client.connect(options);
   });
+  assert.equal(log.length, 2);
+  assert.equal(log[0].events.filter(e => e.request && e.request.indexOf(options.user) !== -1).length, 1);
+  assert.equal(log[0].events.filter(e => e.request && e.request.indexOf(options.password) !== -1).length, 1);
 });
